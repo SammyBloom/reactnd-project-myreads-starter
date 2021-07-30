235320fe8bf2893357accc913705b625f81dba0f
SammyBloom <nwachiemi@gmail.com>
2021-07-30 12:45:52 +0100

    Categorizing done and you can change the category of books

---
+++ src/ListBook.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import Book from './Book'
+
+class ListBook extends Component {
+    render() {
+        const {books} = this.props
+
+        return (
+            <div className="list-books">
+
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+
+                <div className="list-books-content">
+
+                    <div>
+
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">CURRENTLY READING</h2>
+                            <div className="bookshelf-books">
+                                <ol className="books-grid">
+                                    {books.filter((book) => {return book.shelf === 'currentlyReading'}).map((book) => (
+                                        <Book thisBook={book} bookShelf={book.shelf}/>
+                                    ))}
+                                </ol>
+                            </div>
+                        </div>
+
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">WANT TO READ</h2>
+                            <div className="bookshelf-books">
+                                <ol className="books-grid">
+                                    {books.filter((book) => {return book.shelf === 'wantToRead'}).map((book) => (
+                                        <Book thisBook={book} bookShelf={book.shelf}/>
+                                    ))}
+                                </ol>
+                            </div>
+                        </div>
+
+                        <div className="bookshelf">
+                            <h2 className="bookshelf-title">READ</h2>
+                            <div className="bookshelf-books">
+                                <ol className="books-grid">
+                                    {books.filter((book) => {return book.shelf === 'read'}).map((book) => (
+                                        <Book thisBook={book} bookShelf={book.shelf}/>
+                                    ))}
+                                </ol>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        )
+    }
+} 
+
+export default ListBook
